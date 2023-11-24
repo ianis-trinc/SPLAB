757256d26cd24b806ea51b56a0dbc0d60738fdaf
ianis-trinc <ianis.trinc02@e-uvt.ro>
2023-11-24 12:55:27 +0200

    Update

--- SPLAB/SPLAB/Program.cs
+++ SPLAB/SPLAB/Program.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SPLAB
+﻿namespace SPLAB
 {
     public class Program
     {
-        private static void Main(string[] args)
+        private static void Main()
         {
             var noapteBuna = new Book("Noapte buna, copii!");
             var rpGheo = new Author("Radu Pavel Gheo");
