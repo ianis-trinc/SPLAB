aba0ffd6d9c667b9b2482f998b7f588dafec0842
Ianis Trinc <92986508+ianis-trinc@users.noreply.github.com>
2023-11-26 16:05:02 +0200

    Implement Strategy

---
+++ SPLAB/SPLAB/AlignRight.cs
@@ -0,0 +1,18 @@
+﻿using DocumentFormat.OpenXml.InkML;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SPLAB
+{
+    internal class AlignRight : IAlignStrategy
+    {
+        public void Render(Paragraph paragraph)
+        {
+            Paragraph newParagraph = new Paragraph($"--------{paragraph.Text}");
+            paragraph.Text = newParagraph.Text;
+        }
+    }
+}
