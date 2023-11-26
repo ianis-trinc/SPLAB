1da3ea672f52fcc5e1a54cebce26a927b4b24964
Ianis Trinc <92986508+ianis-trinc@users.noreply.github.com>
2023-11-26 13:09:49 +0200

    Update

--- SPLAB/SPLAB/Picture.cs
+++ SPLAB/SPLAB/Picture.cs
@@ -1,11 +1,4 @@
-﻿using DocumentFormat.OpenXml.Drawing.ChartDrawing;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SPLAB
+﻿namespace SPLAB
 {
     internal struct Dimension { public float height; public float width; }
 
